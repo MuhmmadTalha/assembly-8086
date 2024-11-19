6f55a59fb77a1c0fdda52b2a8f6dbbce2b7772a5
Muhammad Talha <104964352+MuhmmadTalha@users.noreply.github.com>
2024-11-19 16:29:23 +0500

    Added new file
    Takes string from user and converts lower case to capital and viceversa

---
+++ case-conversion.asm
@@ -0,0 +1,58 @@
+.model
+.stack 100h
+.data
+buffer db 50 dup('$') 
+result db 50 dup('$')                                        
+.code
+main proc    
+    mov ax,@data
+    mov ds,ax      
+    
+    mov bx,0
+      
+inp:
+    mov ah,01
+    int 21h
+    
+    cmp al, '$'
+    je case1
+    
+    mov buffer [bx],al
+    inc bx
+    jmp inp  
+       
+  
+    
+case1:
+    ;Case 1 - lower to upper
+    
+    cmp al, 'A'
+    jmp case2
+    
+    cmp al,'Z'
+    jmp case2    
+ 
+    sub al,20h
+    
+    
+case2:
+    cmp al, 'a'
+    add al,20h
+    cmp al , 'z'
+    add al,20h
+
+return:
+    mov result[bx],al
+    inc bx 
+    mov dx,offset result
+    mov ah,09h
+    int 21h
+             
+ 
+mov ah,4ch
+int 21h
+    
+main endp     
+end main
+    
+    
